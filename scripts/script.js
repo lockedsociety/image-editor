83435cc3bee9430032f620c5c43b795e8d59c3b1
Sujauddin <61101752+sujaudd1n@users.noreply.github.com>
2023-08-11 13:00:07 +0530

    Add more filters and refactoring

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,11 @@
-// needs refactoring
-
 const file_field = document.querySelector("#file_field");
 const canvas = document.querySelector("#canvas");
 const ctx = canvas.getContext("2d");
 
-const blur = document.querySelector("#blur");
-const brightness = document.querySelector("#brightness");
-const sepia = document.querySelector("#sepia");
-const saturate = document.querySelector("#saturate");
+const filter_inputs = document.querySelectorAll("[type='range']");
 
-const img = new Image();
-let filter = new Map();
+const image = new Image();
+const filter_settings = new Map();
 
 file_field.addEventListener("input", (e) => {
     const image_file = e.target.files[0];
@@ -22,71 +17,68 @@ file_field.addEventListener("input", (e) => {
 });
 
 function runner(e) {
-    img.src = e.target.result;
-    img.onload = () => {
+    image.src = e.target.result;
+    image.onload = () => {
         update_canvas();
         update_canvas_image();
-
-        blur.addEventListener("input", (e) => {
-            manage_blur(e);
-        });
-        brightness.addEventListener("input", (e) => {
-            manage_brightness(e);
-        });
-        sepia.addEventListener("input", (e) => {
-            manage_sepia(e);
-        });
-        saturate.addEventListener("input", (e) => {
-            manage_saturate(e);
-        });
+        reset_filters();
     };
 }
 
-function manage_blur(e) {
-    const blur_amount = e.target.value;
-    filter.set("blur", `blur(${blur_amount}px)`);
+filter_inputs.forEach((e) => {
+    e.addEventListener("input", manage_slider_change);
+});
+
+function manage_slider_change(e) {
+    const filter_name = e.target.name;
+    const filter_value = e.target.value;
+    const filter_unit = e.target.dataset.unit;
+
+    change_filter_settings(filter_name, filter_value, filter_unit);
     update_canvas_image();
 }
 
-function manage_brightness(e) {
-    const blur_amount = e.target.value;
-    filter.set("brightness", `brightness(${blur_amount}%)`);
-    update_canvas_image();
-}
-
-function manage_sepia(e) {
-    const blur_amount = e.target.value;
-    filter.set("sepia", `sepia(${blur_amount}%)`);
-    update_canvas_image();
-}
-function manage_saturate(e) {
-    const blur_amount = e.target.value;
-    filter.set("saturate", `saturate(${blur_amount}%)`);
-    update_canvas_image();
+function change_filter_settings(filter_name, filter_value, filter_unit) {
+    filter_settings.set(
+        filter_name,
+        `${filter_name}(${filter_value}${filter_unit})`
+    );
 }
 
 function update_canvas() {
-    canvas.width = img.width;
-    canvas.height = img.height;
-    filter.clear();
+    canvas.width = image.width;
+    canvas.height = image.height;
 }
 
 function update_canvas_image() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     let filter_string = "";
 
-    filter.forEach((e) => {
+    filter_settings.forEach((e) => {
         filter_string += e + " ";
     });
+    console.log(filter_string)
 
-    ctx.filter = filter_string;
-    ctx.drawImage(img, 0, 0);
+    ctx.filter = filter_string || "none";
+    ctx.drawImage(image, 0, 0);
+}
+
+function reset_filters() {
+    filter_inputs.forEach((e) => {
+        e.value = e.dataset.default;
+    });
+    filter_settings.clear();
 }
 
 document.querySelector(".download").onclick = () => {
-    const a = document.createElement('a');
+    const a = document.createElement("a");
     a.href = canvas.toDataURL();
     a.download = "opensociety_image_editor" + new Date().toLocaleTimeString();
     a.click();
     a.remove();
+};
+
+document.querySelector("#reset_filters").onclick = () => {
+    reset_filters();
+    update_canvas_image();
 };
